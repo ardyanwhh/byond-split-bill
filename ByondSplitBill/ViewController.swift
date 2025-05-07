9930f9b77db276ac82b59ab6dc4c4406d864856c
Ardyan Atmojo <atmojo.ard@gmail.com>
2025-05-07 11:47:18 +0700

    Initial Commit

---
+++ ByondSplitBill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByondSplitBill
+//
+//  Created by Ardyan Atmojo on 07/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
